cdc2df558cd4803df86330c6633d010afee7af41
Kimberly Heidorn <heidorkn@gmail.com>
2019-06-10 20:56:48 -0700

    part of the large palindromeCheck function

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ function addTwo(array, target){
 }
 // const array = [2, 11, 15, 7, 3]
 // addTwo(array, 9)
+
+
+function palindromeCheck(string) {
+  let array = string.split(" ")
+  let newString = array.join("")
+  let obj = {}
+
+  for(let i = 0; i < newString.length; i++) {
+    if(obj[newString[i]] !== undefined){
+      obj[newString[i]] += 1
+    } else {
+      obj[newString[i]] = 1
+    }
+  }
+
+  let values = Object.values(obj)
