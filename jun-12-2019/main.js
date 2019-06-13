e1c1feaa25cc0802646fbaa4d87d451ebf542a20
Kimberly Heidorn <heidorkn@gmail.com>
2019-06-12 18:58:46 -0700

    triangle checkyu7

--- jun-12-2019/main.js
+++ jun-12-2019/main.js
@@ -27,3 +27,36 @@ function tickets(peopleInLine){
   };
   return "YES";
 }
+
+function isValidWalk(walk) {
+  xx = 0
+  yy = 0
+  count = walk.length
+
+  if (count === 10) {
+    for (let i = 0; i < count; i++) {
+      let direction = walk[i]
+
+      if (direction === 'e') {
+        xx++
+      } else if (direction === 'w') {
+        xx--
+      } else if (direction === 'n') {
+        yy++
+      } else {
+        yy--
+      }
+    }
+    if (xx === 0 & yy === 0) {
+      return "true"
+    }
+  };
+}
+
+function isTriangle(a,b,c)
+{
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false
+  }
+  return true
+}
